75adda896c093145a650f75a52ea7e0d58e752f7
kakeru.watanabe <jueda1104@gmail.com>
2019-02-17 08:29:44 +0000

    Userのvalidation, Routing設定 #2

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name, presence:true, lenght: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :email_confirmation, presence: true
+  validates :date_of_birth, presence: true, :future_NG
+  has_seccure_password
+  
+  def future_NG
+    if date_of_birth.present? && birthday > Date.today
+      errors.add(:birthday, "あなたは未来人ですか？")
+    end
+  end
+end
