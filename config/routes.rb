482088983698d70a86c0c2ef954bf0157d2a81eb
kakeru.watanabe <jueda1104@gmail.com>
2019-02-18 02:38:30 +0000

    ユーザー詳細ページ #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
-  get 'users/edit'
-
-  get 'users/update'
-
   root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
