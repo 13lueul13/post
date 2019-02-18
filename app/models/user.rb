688346bf893979c29bb858c06c088d3b0c3315af
kakeru.watanabe <jueda1104@gmail.com>
2019-02-18 06:26:44 +0000

    ユーザーコメント編集ページ #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :email_confirmation, presence: true
-  validates :date_of_birth, presence: true, :future_NG
+  validates :future_NG
   has_seccure_password
   
   def future_NG
